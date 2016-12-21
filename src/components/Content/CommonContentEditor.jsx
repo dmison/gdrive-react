ed8233d5ee3baa5f20db8474ab3ed4a8735598b1
D Mison <dmison@me.com>
2016-12-21 14:53:59 +1000

    refactored toolbar out to own component

--- src/components/Content/CommonContentEditor.jsx
+++ src/components/Content/CommonContentEditor.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Editor, EditorState, RichUtils, convertToRaw, convertFromRaw} from 'draft-js';
+import ContentEditorToolBar from './ContentEditorToolBar.jsx';
 
 class CommonContentEditor extends React.Component {
   constructor(props){
@@ -78,77 +79,6 @@ CommonContentEditor.propTypes = {
   moveContent: React.PropTypes.func
 };
 
-
-
-const buttonBlockStyle = {
-  paddingRight: 10
-};
-
-const toolBarStyle = {
-  backgroundColor: 'grey',
-  padding: 5,
-  marginBottom: 6
-};
-
-const INLINE_STYLES = [
-  {label: 'Bold', style: 'BOLD', icon: 'BOLD'},
-  {label: 'Italic', style: 'ITALIC', icon: 'ITALIC'},
-  {label: 'Underline', style: 'UNDERLINE', icon: 'UNDERLINE'},
-  {label: 'Monospace', style: 'CODE', icon: 'CODE'}
-];
-
-const BLOCK_TYPES = [
-  {label: 'H1', style: 'header-one', icon: 'header-one'},
-  {label: 'H2', style: 'header-two', icon: 'header-two'},
-  {label: 'H3', style: 'header-three', icon: 'header-three'},
-  // {label: 'H4', style: 'header-four', icon: 'header-four'},
-  // {label: 'H5', style: 'header-five', icon: 'header-five'},
-  // {label: 'H6', style: 'header-six', icon: 'header-six'},
-  {label: 'Blockquote', style: 'blockquote', icon: 'blockquote'},
-  {label: 'UL', style: 'unordered-list-item', icon: 'unordered-list-item'},
-  {label: 'OL', style: 'ordered-list-item', icon: 'ordered-list-item'},
-  {label: 'Code Block', style: 'code-block', icon: 'code-block'},
-];
-
-
-const ToolBar = (props) => {
-
-  return (
-    <div className="toolbar" style={toolBarStyle}>
-      <div className='controls'>
-        <span style={buttonBlockStyle}>
-          <button onClick={props._delete}>Del</button>
-        </span>
-        <span style={buttonBlockStyle}>
-          <button onClick={props._moveUp}>Up</button>
-          <button onClick={props._moveDown}>Down</button>
-        </span>
-        <span style={buttonBlockStyle}>
-          {INLINE_STYLES.map((style, index)=>{
-            return <button key={index} onClick={()=>{ props._toggleInlineStyle(style.style);} }>{style.label}</button>;
-          })}
-        </span>
-        <span style={buttonBlockStyle}>
-          <span style={buttonBlockStyle}>
-            {BLOCK_TYPES.map((type, index)=>{
-              return <button  key={index} onClick={()=>{ props._toggleBlockType(type.style);} }>{type.label}</button>;
-            })}
-          </span>
-        </span>
-        <span style={{float: 'right', backgroundColor:'lightgreen', padding: 2, margin: 1}}>{props.type}</span>
-      </div>
-  </div>
-
-  );
-};
-ToolBar.propTypes = {
-  type: React.PropTypes.string,
-  _toggleBlockType: React.PropTypes.func,
-  _toggleInlineStyle: React.PropTypes.func,
-  _delete: React.PropTypes.func,
-  _moveUp: React.PropTypes.func,
-  _moveDown: React.PropTypes.func,
-};
 
 
 export default CommonContentEditor;
---
+++ src/components/Content/ContentEditorToolBar.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+const buttonBlockStyle = {
+  paddingRight: 10
+};
+
+const INLINE_STYLES = [
+  {label: 'Bold', style: 'BOLD', icon: 'bold'},
+  {label: 'Italic', style: 'ITALIC', icon: 'italic'},
+  {label: 'Underline', style: 'UNDERLINE', icon: 'underline'},
+  {label: 'Monospace', style: 'CODE', icon: 'code'}
+];
+
+const BLOCK_TYPES = [
+  {label: 'H1', style: 'header-one', icon: 'header'},
+  {label: 'H2', style: 'header-two', icon: 'header'},
+  {label: 'H3', style: 'header-three', icon: 'header'},
+  // {label: 'H4', style: 'header-four', icon: 'header-four'},
+  // {label: 'H5', style: 'header-five', icon: 'header-five'},
+  // {label: 'H6', style: 'header-six', icon: 'header-six'},
+  {label: 'Blockquote', style: 'blockquote', icon: 'quote-right'},
+  {label: 'UL', style: 'unordered-list-item', icon: 'list-ul'},
+  {label: 'OL', style: 'ordered-list-item', icon: 'list-ol'},
+  // {label: 'Code Block', style: 'code-block', icon: 'code-block'},
+];
+
+const ContentEditorToolBar = (props) => {
+
+  return (
+    <div className="toolbar" >
+        <div className='btn-group' style={buttonBlockStyle}>
+          <a className='btn btn-danger btn-sm' onClick={props._delete}><i className='fa fa-trash' title='Delete this content (unrecoverable).'/></a>
+        </div>
+        <div className='btn-group' style={buttonBlockStyle}>
+          <a className='btn btn-default btn-sm' onClick={props._moveUp}><i className='fa fa-chevron-up' title='Move content block up.'/></a>
+          <a className='btn btn-default btn-sm' onClick={props._moveDown}><i className='fa fa-chevron-down' title='Move content block down.'/></a>
+        </div>
+        <div className='btn-group' style={buttonBlockStyle}>
+          {INLINE_STYLES.map((style, index)=>{
+            return <ContentEditorToolBarButton key={index}  type={style} active={props.currentInlineStyle.has(style.style)} _toggleEffect={props._toggleInlineStyle} />;
+          })}
+      </div>
+      <div className='btn-group' style={buttonBlockStyle}>
+          {BLOCK_TYPES.map((type, index)=>{
+            return <ContentEditorToolBarButton key={index} active={type.style===props.currentBlockType} type={type} _toggleEffect={props._toggleBlockType} />;
+          })}
+      </div>
+      <span style={{float: 'right', margin: 0, padding: 7}} className='label label-default'>{props.type}</span>
+  </div>
+
+  );
+};
+
+ContentEditorToolBar.propTypes = {
+  type: React.PropTypes.string,
+  currentInlineStyle: React.PropTypes.object,
+  currentBlockType: React.PropTypes.string,
+  _toggleBlockType: React.PropTypes.func,
+  _toggleInlineStyle: React.PropTypes.func,
+  _delete: React.PropTypes.func,
+  _moveUp: React.PropTypes.func,
+  _moveDown: React.PropTypes.func,
+};
+
+
+const ContentEditorToolBarButton = (props) => {
+  const active = props.active? ' active':'';
+  return (
+    <a className={'btn btn-default btn-sm'+active}
+        onClick={()=>{ props._toggleEffect(props.type.style);}}>
+          <i  className={'fa fa-'+props.type.icon}>{props.type.icon === 'header' ?<span >{props.type.label.substring(1)}</span>:''}</i>
+      </a>
+  );
+};
+ContentEditorToolBarButton.propTypes = {
+  _toggleEffect: React.PropTypes.func,
+  type: React.PropTypes.object,
+  active: React.PropTypes.bool
+};
+
+export default ContentEditorToolBar;
