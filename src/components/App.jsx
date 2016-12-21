2638a35289a58fb2294f48af74b19b9065c69084
D Mison <dmison@me.com>
2016-12-21 15:16:34 +1000

    navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {name} from '!json!../../package.json';
 
 const App = (props) => {
   return (
     <div>
-      <h1>Email Thingy</h1>
-        <Link to=''>home</Link>
-      <Link to='mailings'>Mailings</Link>
 
-      {props.children}
+      <nav className='navbar navbar-default'>
+        <div className='container-fluid'>
+          <div className='navbar-header'>
+            <a className='navbar-brand' href='#'> {name} </a>
+          </div>
+          <ul className='nav navbar-nav'>
+            <li><Link to=''>home</Link></li>
+            <li><Link to='mailings'>Mailings</Link></li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className='container'>
+        {props.children}
+      </div>
     </div>
   );
 
