e0e0041c8fd511d315f69651c87912c5729b5efb
D Mison <dmison@me.com>
2017-02-21 15:45:00 +1000

    handling {{email}} and {{name}} in emails now, also much better statuses

--- src/components/Email/EmailSender.jsx
+++ src/components/Email/EmailSender.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 var Base64 = require('js-base64').Base64;
 import contentForRecipient from '../Content/ContentForRecipient.js';
-import {getEmail, isValid} from '../Recipient/utils.js';
+import {getEmail, isValid, getName} from '../Recipient/utils.js';
 import Modal from 'react-bootstrap/lib/Modal';
 import Alert from 'react-bootstrap/lib/Alert';
 import eachOfLimit from 'async/eachOfLimit';
+import Handlebars from 'handlebars';
 
 class EmailSender extends React.Component {
   constructor(props){
@@ -18,12 +19,28 @@ class EmailSender extends React.Component {
   }
 
   componentWillReceiveProps(newProps){
+
     if(newProps.show && !this.props.show){
-      this.setState({status: this.props.recipients.filter((r)=>{ return isValid(r.detail); }).map((recipient)=>{
+      this.setState({status: this.props.recipients.map((recipient)=>{
+        const content = contentForRecipient(this.props.content, recipient.id).map((c)=>{
+          return c===''?'':`${c}\n\n`;
+        }).join('\r\n\r\n');
+        let compiledBody = '';
+        let contentError = false;
+        try{
+          const builder = Handlebars.compile(content);
+          compiledBody = builder({ email:getEmail(recipient.detail), name:getName(recipient.detail) });
+        } catch (e) {
+          compiledBody = content;
+          contentError = true;
+        }
         return {
           id: recipient.id,
           detail: recipient.detail,
-          sent: ''
+          sent: '',
+          email: `To: ${getEmail(recipient.detail)}\r\nSubject: ${this.props.subject}\r\n\r\n${compiledBody}`,
+          contentValid: !contentError,
+          emailValid: isValid(recipient.detail)
         };
       }), sending: false});
 
@@ -33,13 +50,19 @@ class EmailSender extends React.Component {
 
   render(){
 
-    const invalidStyle = {
-      backgroundColor: 'red',
-      color: 'white',
-      padding: '3px 7px',
-      borderRadius: '3px'
+    const headerStyle = {
+      // borderBottom: '1px solid #f0f0f0',
+      paddingBottom: '5px',
+      paddingTop: '0px'
     };
-    const areInvalidRecipients = this.props.recipients.length > this.state.status.length;
+
+    const validRecipients = this.state.status.filter((s)=>{ return s.emailValid && s.contentValid; });
+    const invalidRecipients = this.state.status.filter((s)=>{ return !s.emailValid; });
+    const contentErrors = this.state.status.filter((s)=>{ return !s.contentValid; });
+
+    const areValidRecipients = validRecipients.length > 0;
+    const areInvalidRecipients = invalidRecipients.length > 0;
+    const areContentErrors = contentErrors.length > 0;
 
     return (
       <Modal show={this.props.show}>
@@ -55,7 +78,7 @@ class EmailSender extends React.Component {
             <button
               className='btn btn-success'
               style={{fontSize:12, paddingBottom: 5, margin: '0px 10px 0px 0px', float: 'right'}}
-              disabled={!this.context.gapi || this.state.status.length === 0 || this.state.sending}
+              disabled={!this.context.gapi || !areValidRecipients}
               onClick={this._sendEmail}>
                 {this.state.sending? 'Sending ':'Send '}
                 {this.state.sending? <i className="fa fa-circle-o-notch fa-spin" aria-hidden="true"></i>:<i className='fa fa-paper-plane-o' aria-hidden='true'></i>}
@@ -70,35 +93,55 @@ class EmailSender extends React.Component {
         </Modal.Header>
         <Modal.Body>
 
-          {this.context.gapi === null?<Alert bsStyle='danger'>
+          <Alert bsStyle='danger' style={{ display: this.context.gapi === null? 'block':'none'}}>
             <p><strong>Google API not initialized!</strong> You won't be able to send email.</p>
-            <p>Workaround: go back to <a href='#mailings'>#/mailings</a>, refresh the page and wait until login/logout button appears.</p>
-          </Alert>:''}
+            <p><strong>Workaround:</strong> Go back to <a href='#mailings'>#/mailings</a>, refresh the page and wait until login/logout button appears.</p>
+          </Alert>
 
-          <h4>{areInvalidRecipients?'Valid ':''}Recipients {areInvalidRecipients?<small>{this.state.status.length} valid of {this.props.recipients.length}</small>:''}</h4>
-          <ul style={{listStyleType: 'none', paddingLeft: 0}}>
-            {this.state.status.map((r)=>{
-              return (
-                <li key={r.id}>
-                  {r.detail} <ShowStatus msg={r.sent} />
-                </li>
-              );
-            })}
-          </ul>
+          <Alert bsStyle='danger' style={{ display: areValidRecipients? 'none':'block'}}>
+            <p><strong>You have no valid emails to send!</strong></p>
+          </Alert>
 
-          {areInvalidRecipients?<div style={{marginTop: '20px'}}>
-            <h4>{this.props.recipients.length - this.state.status.length} Invalid Recipient{this.props.recipients.length - this.state.status.length>1?'s':''} <small> No email will be sent to {this.props.recipients.length - this.state.status.length>1?'these':'it'}.</small></h4>
+
+          <div style={{marginTop: '0px', display: areValidRecipients? 'block':'none', padding: '1px 10px 1px 10px'}}>
+            <h4 style={headerStyle}>{areInvalidRecipients?'Valid ':''}Recipients {areInvalidRecipients?<small>{validRecipients.length} valid of {this.props.recipients.length}</small>:''}</h4>
             <ul style={{listStyleType: 'none', paddingLeft: 0}}>
-              {this.props.recipients.filter((r)=>{ return !isValid(r.detail); }).map((r)=>{
+              {validRecipients.map((r)=>{
                 return (
-                  <li key={r.id} >
-                    <span style={invalidStyle}>{r.detail}</span>
+                  <li key={r.id}>
+                    {r.detail} <ShowStatus msg={r.sent} />
                   </li>
                 );
-
               })}
             </ul>
-          </div>:''}
+          </div>
+
+          <div style={{backgroundColor: '#f2dede', marginTop: '20px',display: areContentErrors?'block':'none', borderRadius: '0px', borderLeft: '10px solid #d9534f', padding: '1px 10px 1px 10px'}}>
+            <h4 style={headerStyle}>Content Errors <small>{contentErrors.length} recipient{contentErrors.length>1?'s':''} with invalid content. No email will be sent to {contentErrors.length>1?'these':'it'}.</small></h4>
+            <ul style={{listStyleType: 'none', paddingLeft: 0}}>
+              {contentErrors.map((r)=>{
+                return (
+                  <li key={r.id}>
+                    {r.detail}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+          <div style={{backgroundColor: '#f2dede',marginTop: '20px',display: areInvalidRecipients?'block':'none', borderRadius: '0px', borderLeft: '10px solid #d9534f', padding: '1px 10px 1px 10px'}}>
+            <h4 style={headerStyle}>Invalid Email <small>{invalidRecipients.length} recipient{invalidRecipients.length>1?'s':''} with {invalidRecipients.length>1?'':'an '}invalid email address{invalidRecipients.length>1?'ess':''}. No email will be sent to {invalidRecipients.length>1?'these':'it'}.</small></h4>
+            <ul style={{listStyleType: 'none', paddingLeft: 0}}>
+              {invalidRecipients.map((r)=>{
+                return (
+                  <li key={r.id} >
+                    {r.detail}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
         </Modal.Body>
       </Modal>
     );
@@ -110,43 +153,30 @@ class EmailSender extends React.Component {
         s.sent = 'waiting';
         return s;
       })});
-      const emails = this.state.status.map((r)=>{
-        return {
-          recipient: r.id,
-          msg: `To: ${getEmail(r.detail)}\r\nSubject: ${this.props.subject}\r\n\r\n${contentForRecipient(this.props.content, r.id).join('\r\n\r\n')}`
-        };
-      });
+      const emails = this.state.status.filter((s)=>{ return s.emailValid && s.contentValid; });
 
       eachOfLimit(
         emails,
         2,
         (email, index, callback)=>{
           if(this.state.abort){
-            this.setState({status: this.state.status.map((s)=>{
-              s.sent = (s.id === email.recipient)? 'aborted': s.sent;
-              return s;
-            })}, callback());
+            // mark as aborted
+            this.setState({status: this.state.status.map((s)=>{ s.sent = (s.id === email.id)? 'aborted': s.sent; return s; })}, ()=>{ callback(); });
           } else {
-            const base64EncodedMsg = Base64.encodeURI(email.msg);
-            this.setState({status: this.state.status.map((s)=>{
-              s.sent = (s.id === email.recipient)? 'sending': s.sent;
-              return s;
-            })});
+            const base64EncodedMsg = Base64.encodeURI(email.email);
+            // mark as sending
+            this.setState({status: this.state.status.map((s)=>{ s.sent = (s.id === email.id)? 'sending': s.sent; return s; })});
             this.context.gapi.client.gmail.users.messages.send({
               'userId': 'me',
               'resource': {
                 'raw': base64EncodedMsg
               }
             }).then(()=>{
-              this.setState({status: this.state.status.map((s)=>{
-                s.sent = (s.id === email.recipient)? 'yes': s.sent;
-                return s;
-              })}, callback());
+              // success
+              this.setState({status: this.state.status.map((s)=>{ s.sent = (s.id === email.id)? 'yes': s.sent; return s; })}, ()=>{ callback(); });
             }, (reason)=>{
-              this.setState({status: this.state.status.map((s)=>{
-                s.sent = (s.id === email.recipient)? reason.result.error.message: s.sent;
-                return s;
-              })}, callback());
+              // failure
+              this.setState({status: this.state.status.map((s)=>{ s.sent = (s.id === email.id)? reason.result.error.message: s.sent; return s; })}, ()=>{ callback(); });
             });
           }
         },
